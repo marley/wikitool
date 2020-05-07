b00eab97f70d49cddd1e3752bcdc4d6777ff30ec
Marley Alford <marleycalford@gmail.com>
2020-05-07 15:54:10 -0400

    WIP - new fetch structure, trying to fetch both titles and associated images

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const handleAsJson = (resp) => resp.json();
+const map = (f) => (xs) => xs.map(f);
+const all = Promise.all.bind(Promise);
+let wikiUrl =
+  "https://tools.wmflabs.org/massviews/api.php?project=en.wikipedia.org&category=Wikipedia%20requested%20logos&limit=20000";
+const getImageList = ({ title }) =>
+  `https://cors-anywhere.herokuapp.com/http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=${title}`;
+
 function createNode(element) {
   return document.createElement(element); // Create the type of element you pass in the parameters
 }
@@ -7,21 +15,51 @@ function append(parent, el) {
 }
 
 const ul = document.getElementById("pages"); // Get the list where we will place our pages
-let wikiUrl =
-  "https://tools.wmflabs.org/massviews/api.php?project=en.wikipedia.org&category=Wikipedia%20requested%20logos&limit=20000";
 
-fetch(`https://cors-anywhere.herokuapp.com/${wikiUrl}`)
-  .then((response) => response.json())
-  .then(function (data) {
-    let pages = data;
-    return pages.map(function (page) {
+const createListItem = ({ title }) =>
+  fetch(
+    `https://cors-anywhere.herokuapp.com/http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=${title}`
+  )
+    .then(handleAsJson)
+    .then(function (data) {
+      console.log({ title, data });
       let li = createNode("li"),
+        img = createNode("img"),
         span = createNode("span");
-      span.innerHTML = `<a href=https://en.wikipedia.org/wiki/${page.title}>${page.title}</a>`;
+      span.innerHTML = `<a href=https://en.wikipedia.org/wiki/${title}>${title}</a>`;
+      img.src = data[0];
       append(li, span);
+      append(li, img);
       append(ul, li);
     });
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+// let wikiUrl =
+//   "https://tools.wmflabs.org/massviews/api.php?project=en.wikipedia.org&category=Wikipedia%20requested%20logos&limit=20000";
+
+// fetch(`https://cors-anywhere.herokuapp.com/${wikiUrl}`)
+// .then((response) => response.json())
+// .then(function (data) {
+//   let pages = data;
+//   return pages.map(function (page) {
+//     let li = createNode("li"),
+//       img = createNode("img"),
+//       span = createNode("span");
+//     img.innerHTML = fetch(
+//       `https://cors-anywhere.herokuapp.com/http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=${page.title}`
+//     )
+//       .then((response) => response.json())
+//       .then(function (imgData) {
+//         imgData;
+//       });
+//     span.innerHTML = `<a href=https://en.wikipedia.org/wiki/${page.title}>${page.title}</a>`;
+//     append(li, span);
+//     append(li, img);
+//     append(ul, li);
+//   });
+// })
+// .catch(function (error) {
+//   console.log(error);
+// });
+
+fetch(`https://cors-anywhere.herokuapp.com/${wikiUrl}`)
+  .then(handleAsJson)
+  .then(map(createListItem));
